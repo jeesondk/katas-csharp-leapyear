4c347502a142e31e185f196710ece23ee45cea83
Jesper Jensen <jesper.j@maersk.com>
2023-03-25 16:38:22 +0100

    Feat: added implementation for divisible by 400

--- LeapYearCalculator/Calculator.cs
+++ LeapYearCalculator/Calculator.cs
@@ -6,8 +6,9 @@ public class Calculator
     {
         var divisibleBy4 = year % 4 == 0;
         var divisibleBy100 = year % 100 == 0;
+        var divisibleBy400 = year % 400 == 0;
 
         if (!divisibleBy4) return false;
-        return !divisibleBy100;
+        return !divisibleBy100 || divisibleBy400;
     }
 }
