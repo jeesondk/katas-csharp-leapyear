250be7d14b1cbb29dcfdbc555bf1b6b943dfe8c7
Jesper Jensen <jesper.j@maersk.com>
2023-03-25 16:34:26 +0100

    Refactor: removed unused code

--- LeapYearCalculator/Calculator.cs
+++ LeapYearCalculator/Calculator.cs
@@ -7,8 +7,6 @@ public class Calculator
         var divisibleBy4 = year % 4 == 0;
         var divisibleBy100 = year % 100 == 0;
 
-        var specialRule100 = divisibleBy4 && divisibleBy100;
-
         if (!divisibleBy4) return false;
         return !divisibleBy100;
     }
